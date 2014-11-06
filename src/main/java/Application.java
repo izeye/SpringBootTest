a46fba1cdabcba662adeef3a13cae439f3ce4931
izeye <izeye@naver.com>
2014-11-06 23:38:48 +0900

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by izeye on 2014. 11. 6..
+ */
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
